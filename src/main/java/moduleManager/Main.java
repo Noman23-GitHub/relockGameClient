3c2643915e1cc7d24a38d57fd21fe1aa0e131c6f
SaltyManGit <Salty-MNA@yandex.ru>
2017-07-10 17:28:49 +0400

    gov

--- src/main/java/moduleManager/Main.java
+++ src/main/java/moduleManager/Main.java
@@ -2,7 +2,7 @@ package moduleManager;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Salty loh");
+        System.out.println("Salty boh");
         System.out.println("++++");
         System.out.println("Commit 4");
         System.out.println("Commit 5");
