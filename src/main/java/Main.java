56d54038604d5db7f1d1ea44ecc1067055a23401
Nikita Tretyakov <nomascript@gmail.com>
2017-07-10 17:05:57 +0400

    Commit 4

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Salty loh");
         System.out.println("++++");
+        System.out.println("Commit 4");
     }
 }
