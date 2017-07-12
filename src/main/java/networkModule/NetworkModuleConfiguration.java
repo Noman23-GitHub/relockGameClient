d864b59838f79862917cfbd4309687bcfdf50e0c
zebra47 <forbelor@gmail.com>
2017-07-12 15:47:14 +0400

    commit №2

--- src/main/java/networkModule/NetworkModuleConfiguration.java
+++ src/main/java/networkModule/NetworkModuleConfiguration.java
@@ -3,16 +3,39 @@ package networkModule;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
 import org.springframework.core.task.SimpleAsyncTaskExecutor;
 import org.springframework.core.task.TaskExecutor;
 
+import javax.annotation.Resource;
+import java.net.Socket;
+
 @Configuration
 @ComponentScan
+@PropertySource("classpath:networkSettings.properties")
 public class NetworkModuleConfiguration {
+
+    @Resource
+    Environment environment;
+
+    @Bean
+    public NetworkSettings networkSettings()
+    {
+        String hostName = environment.getProperty("network.hostname");
+        int port = Integer.parseInt(environment.getProperty("network.port"));
+        return new NetworkSettings(hostName,port);
+    }
 
     @Bean
     public NetworkModule networkModule() {
         return new NetworkModule();
+    }
+
+    @Bean
+    public Socket socket()
+    {
+        return  new Socket();
     }
 
     @Bean
