117e42659205bbbd53d4d5d60d209063647d2b89
Nikita Tretyakov <nomascript@gmail.com>
2017-07-12 10:51:43 +0400

    Небольшая уборка

--- src/main/java/exceptionModule/ExceptionModuleInterface.java
+++ src/main/java/exceptionModule/ExceptionModuleInterface.java
@@ -1,10 +1,6 @@
 package exceptionModule;
 
-/**
- * Created by Student on 11-Jul-17.
- */
 public interface ExceptionModuleInterface {
     public void setServerException(Exception exception);
-
     public void setLocalException(Exception exception);
 }
--- src/main/java/ingameModule/IngameModuleInterface.java
+++ src/main/java/ingameModule/IngameModuleInterface.java
@@ -2,9 +2,6 @@ package ingameModule;
 
 import stateData.GameState;
 
-/**
- * Created by Student on 11-Jul-17.
- */
 public interface IngameModuleInterface {
     public void setGameState(GameState gameState);
 }
